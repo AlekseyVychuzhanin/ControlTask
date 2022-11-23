ca46766f618d176cbbcd7c4398de865e3f80fa5b
Elena <agapitovae87@gmail.com>
2022-11-23 18:30:54 +0300

    Added task solution

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,45 @@
-﻿// 
+﻿Console.Clear();
+
+// string[] stringArray = {"hello", "2", "world",":-)"};
+// string[] stringArray = {"Russia", "Denmark", "Kazan"};
+string[] stringArray = {"1234", "1567", "-2", "computer science"};
+
+string[] CreateNewStringArray(string[] array)
+{
+    int length = 0;
+    for(int i = 0; i < array.Length;i++)
+    {
+         if(array[i].Length <= 3) length++;
+    }
+
+    string[] newArray = new string[length];
+
+    for(int j = 0, m = 0; j < array.Length; j++)
+    {
+        if(array[j].Length <= 3)
+        {
+            newArray[m] = array[j];
+            m++;
+        }
+    }
+    return newArray;
+}
+
+void PrintStringArray(string[] array)
+{
+    Console.Write("Массив строк: [");
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("'" + array[i] + "'");
+        if(i < array.Length -1) Console.Write(",");
+    }
+    Console.Write("].");
+    Console.WriteLine();
+}
+
+PrintStringArray(stringArray);
+string[] newStringArray = CreateNewStringArray(stringArray);
+PrintStringArray(newStringArray);
+
+Console.ReadLine();
